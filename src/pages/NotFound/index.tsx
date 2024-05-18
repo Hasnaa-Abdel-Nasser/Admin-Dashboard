4c6565737d7acc0ef25876fb0e8450477683db5c
Hasnaa Abdel Naser <112397654+Hasnaa-Abdel-Naser@users.noreply.github.com>
2024-05-19 02:09:15 +0300

    not found page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom"
+import { Button } from "../../components/ui/Button"
 
 const NotFoundPage = () => {
+  const navigate = useNavigate();
   return (
-    <div>NotFoundPage</div>
+    <div className="w-screen h-screen bg-white flex flex-col justify-center items-center">
+      <img src="images/404.jpg" className="w-1/4"/>
+      <h1 className="text-green-600 text-4xl">OOPS!</h1>
+      <p className="text-slate-500 text-xs">The page you requested could not be found..</p>
+      <Button className="bg-[#59E4A8] mt-12 text-xs py-3 px-4 rounded-full font-semibold" onClick={()=>navigate("/")}>Back To Home</Button>
+    </div>
   )
 }
 
